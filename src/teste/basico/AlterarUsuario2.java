80f3225b1d5caecd281046f28188e872ee82c4b6
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2021-01-19 18:56:44 -0300

    Alterar Usuário #02

--- src/teste/basico/AlterarUsuario2.java
+++ src/teste/basico/AlterarUsuario2.java
@@ -6,19 +6,20 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class AlterarUsuario1 {
-
+public class AlterarUsuario2 {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
+
+        EntityManagerFactory emf = Persistence
+                .createEntityManagerFactory("exercicios-jpa");
         EntityManager em = emf.createEntityManager();
 
         em.getTransaction().begin();
 
         Usuario usuario = em.find(Usuario.class, 9L);
-        usuario.setNome("Leonardo");
+        usuario.setNome("Leonardo Alterado");
         usuario.setEmail("leonardo@lanche.com.br");
 
-        em.merge(usuario);
+//        em.merge(usuario);
 
         em.getTransaction().commit();
 
